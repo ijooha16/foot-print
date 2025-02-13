be09604e5d9e2d9c836d341fd7f7c4fd9fe3bf66
Juha Yoon <ijooha16@gmail.com>
2025-02-13 13:59:58 +0900

    add: 카드 컴포넌트 추가 및 마이페이지 레이아웃

---
+++ src/components/MypagePostCard.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const MypagePostCard = ({data}) => {
+  return (
+    <PostBox>
+        hi
+        {data.title}
+    </PostBox>
+  )
+}
+
+export default MypagePostCard
+
+const PostBox = styled.div`
+ width: 240px;   
+ height: 240px;
+ /* border-radius: 20px; */
+ background-color: gray;
+`
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import supabase from "../supabase/client";
+import styled from "styled-components";
+import MypagePostCard from "../components/MypagePostCard";
 
 const MyPage = () => {
   const [posts, setPosts] = useState([]);
 
   //데이터 갖다 쓰기
   useEffect(() => {
-    const getTodos = async () => {
+    const getPosts = async () => {
       try {
         const { data, error } = await supabase.from("posts").select("*");
         if (error) throw error;
@@ -15,13 +17,69 @@ const MyPage = () => {
         console.log(error);
       }
     };
-    getTodos();
+    getPosts();
   }, []);
 
   //데이터 넣기
   supabase.from("posts").insert({ posts });
 
-  return <div>MyPage {posts.map(e => e.title)}</div>;
+  return (
+    <>
+      MyPage {posts.map(e => e.title)}
+      <ContentsBox direction="row">
+        <ProfileImg>img</ProfileImg>
+        <div>
+          nickName
+          MBTI
+          email
+          blabla
+        </div>
+      </ContentsBox>
+      <GridBox>
+        {posts.map(data => <Fragment key={data.id}><MypagePostCard data={data}/></Fragment>)}
+      </GridBox>
+    </>
+  );
 };
 
 export default MyPage;
+
+const ContentsBox = styled.div`
+  width: ${props => (props.modal ? "920px" : "800px")};
+  padding: 60px 40px;
+  margin-bottom: 60px;
+  gap: 40px;
+  border: none;
+  border-radius: 60px;
+  background-color: lightgray;
+
+  display: flex;
+  flex-direction: ${props => props.direction || "column"};
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProfileImg = styled.div`
+  width: 180px;
+  height: 180px;
+  border: none;
+  border-radius: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`
+
+const GridBox = styled.div`
+    width: ${props => (props.modal ? "920px" : "800px")};
+  padding: 60px 40px;
+  margin-bottom: 60px;
+  gap: 40px;
+  border: none;
+  border-radius: 60px;
+  background-color: lightgray;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 40px;
+`
