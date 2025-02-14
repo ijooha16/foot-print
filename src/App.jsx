e632c7055344f0c0181d13c094d992a5bcdadfb8
JinChaeKim <rlawlsco88@gmail.com>
2025-02-14 15:00:40 +0900

    add : HomeContext 생성 및 App 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { AuthProvider } from "./context/AuthProvider.jsx";
 import { PostProvider } from "./context/PostContext.jsx";
 import Router from "./shared/Router.jsx";
 import "./App.css";
+import { HomeProvider } from "./context/HomeContext.jsx";
 
 function App() {
   return (
-    <AuthProvider>
-      <PostProvider>
-        <Router />
-      </PostProvider>
-    </AuthProvider>
+    <HomeProvider>
+      <AuthProvider>
+        <PostProvider>
+          <Router />
+        </PostProvider>
+      </AuthProvider>
+    </HomeProvider>
   );
 }
 
