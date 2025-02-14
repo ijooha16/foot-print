286e79bb8601b54f737760f83a494bb17f3a9248
JinChaeKim <rlawlsco88@gmail.com>
2025-02-14 20:07:38 +0900

    add : 공통 컴포넌트 - 게시글 추가 버튼

---
+++ src/components/AddPostButton.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const AddPostButton = () => {
+  return (
+    <>
+      <Link
+        to={"/posting"}
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <StAddBtn>+</StAddBtn>
+      </Link>
+    </>
+  );
+};
+
+export default AddPostButton;
+
+const StAddBtn = styled.div`
+  position: fixed;
+  top: 80%;
+  right: 5%;
+  font-weight: 200;
+  font-size: 250%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 70px;
+  color: white;
+  background-color: #005eff;
+  border-radius: 70%;
+`;
