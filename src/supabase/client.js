fa657287dd652883ecd5387b33540383337b7995
JinChaeKim <rlawlsco88@gmail.com>
2025-02-14 20:56:02 +0900

    fix : client - console 삭제

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -3,8 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_PROJECT_URL = import.meta.env.VITE_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_APP_SUPABASE_ANON_KEY;
 
-console.log(import.meta.env.NEXT_PUBLIC_SUPABASE_URL);
-
 const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
 
 export default supabase;
